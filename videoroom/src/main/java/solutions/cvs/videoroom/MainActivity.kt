3fa9f6c2a93f415a2f5ea9e668481b2e3b6c4326
Alexey Poryadin <alexey.poryadin@gmail.com>
2020-11-12 19:53:11 +0300

    MainActivity; LoginFragment layout

---
+++ videoroom/src/main/java/solutions/cvs/videoroom/MainActivity.kt
@@ -0,0 +1,13 @@
+package solutions.cvs.videoroom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
